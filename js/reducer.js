13670f9673b28c580fd479d7f3db8afb4c20dcf1
jasonemarkson <jasonmarkson76@gmail.com>
2021-08-20 12:56:46 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,6 +1,7 @@
-let state = {count: 0};
+let state
 
-function changeState(state, action){
+// reducer -- function to change the state of variable
+function changeState(state = { count: 0 }, action){
     switch (action.type) {
       case 'INCREASE_COUNT':
         return {count: state.count + 1}
@@ -9,6 +10,7 @@ function changeState(state, action){
     }
   }
 
+  // dispatch -- function to reflect the updated state and render it to the DOM
 function dispatch(action){
     state = changeState(state, action)
     render()
@@ -17,3 +19,5 @@ function dispatch(action){
 function render(){
     document.body.textContent = state.count
 }
+
+dispatch({type: '@@INIT'})
